f0083565861b52483cbd3692396923ba0d4ae6ea
[Github <dltjwls1017@naver.com>
2023-12-05 11:49:08 +0900

    최종

--- seojin_bank.py
+++ seojin_bank.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import random
 import requests
+from bs4 import BeautifulSoup
 import pymysql
 import pandas as pd
 
@@ -53,13 +54,34 @@ try:
                 return True
             
         # 1번 계좌 개설
-        def accountCreate():
-            account = input("계좌 입력: ")
-            if BankManager.accountCheck(account):
-                name = input("이름 입력: ")
-                bal = int(input("첫 금액 입금: "))
-                AccountCreate(account, name, bal)
+        def Create(): # 헷갈려서 이름 변경 
+            # 추천 계좌 생성 
+            first = '%02d'%random.randint(0,99)
+            now = datetime.datetime.now()
+            second = now.strftime("%Y%m%d")
+            third = '%06d'%random.randint(0,999999)
+            recommend = first + "-" + second + "-" + third
+            print("* 원하는 번호로 계좌를 생성 할 수 있습니다.\
+                \n\n 추천 계좌 번호 : \
+                    \n<< {} >>\
+                        \n===========================================".format(recommend))
+            print("* 계속 진행하길 원하면 y , 첫 화면으로 가길 원하면 n 을 입력해주세요.")
+            a = input()
+            if a=="y":
+                account = input(" - 계좌 번호: ")
+                if BankManager.accountCheck(account): # 중복확인
+                    name = input(" - 이름 입력: ")
+                    print("\n* 계좌개설을 위해 소정의 금액을 입금이 필요합니다.\
+                        \n* 얼마를 입금하시겠습니까?\n")
+                    bal = int(input(" - 첫 입금 금액: "))
+                    print("\n* 개설이 완료 되셨습니다. 첫화면으로 이동합니다.")
+                    
+                    # 입력 받은 데이터를 AccountCreate 클래스로 
+                    AccountCreate(account, name, bal)
+            elif a=="n":
+                BankSystem.lobby()        
         
+            
         
         # 2번 입금 
         def addBalance():
@@ -73,8 +95,11 @@ try:
                     for account in bankinform: # 계좌번호 찾기 
                         if inputaccount == account[0]: # 
                             account[2] = int(account[2]) + inputmoney # 계좌번호 잔액에 금액 추가
+                            print("\n* {}원 입금이 완료 되었습니다. 첫화면으로 이동합니다.".format(inputmoney))  # 입금 되었는지 확인          
+                            BankSystem.pyHistoryD(inputmoney)           
                                         
-                                        
+
+
                     # 입금 내역 기록
                     myaccount = inputaccount + ".txt"
                     with open(myaccount, 'a', encoding='UTF-8') as f:
@@ -84,9 +109,12 @@ try:
                         
                 # 계좌번호 없을 경우
                 else:
-                    print("없는 계좌번호 입니다")
+                    print("===== [!] 없는 계좌번호 입니다 =====")
+                    print("* 첫 화면으로 돌아갑니다 ")
+                    
+                BankSystem.lobby() 
             else :
-                print("첫 화면으로 돌아갑니다")
+                BankSystem.lobby() 
 
 
         # 3번 출금
@@ -111,12 +139,13 @@ try:
                                 print("잔액이 모자랍니다")
                                 print("첫화면으로 돌아갑니다")
                         else:
-                            pass
-                else:
+                            pass    # 다음 거 찾아주기 
+                else:    # 없는 계좌면 
                     print("없는 계좌번호 입니다")
                     print("첫화면으로 돌아갑니다")
             else:
                 print("첫 화면으로 돌아갑니다")
+                BankSystem.lobby() 
                 
 
         # 4번 잔액조회
@@ -130,9 +159,13 @@ try:
                     for account in bankinform:
                         if inputaccount == account[0]:
                             print("{} 계좌의 잔액은 {} 원입니다".format(account[0], account[2]))
+                            print("첫 화면으로 돌아갑니다")
+                else:   # 없는 계좌면 
+                    print("없는 계좌번호 입니다")
+                    print("5초 후에 첫 화면으로 돌아갑니다")
             else:
                 print("첫 화면으로 돌아갑니다")
-
+                BankSystem.lobby() 
 
                         
         # 5번 계좌 입출금 이력 조회
@@ -152,8 +185,12 @@ try:
                             print('[{}] {}'.format(cnt,data),end="")
                 else:
                     print("이력이 없습니다")
+                    print("첫 화면으로 돌아갑니다")
             else:
                 print("첫 화면으로 돌아갑니다")
+                BankSystem.lobby() 
+
+
 
         # 환율 .. 어떻게 하징
         def exchangeCurrent( ):
@@ -162,10 +199,29 @@ try:
             a = input()
 
             if a=="y":
-                print("um..")
-            else:
-                print("첫 화면으로 돌아갑니다")
+                address = f'https://finance.naver.com'
+                addition = '/marketindex/?tabSel=exchange#tab_section'
 
+                #환율 테이블 안에서 꺼내와야하므로 2번에 걸쳐서  
+                res = requests.get(address +addition)
+                soup = BeautifulSoup(res.text, 'html.parser')
+                frame = soup.find('iframe', id="frame_ex1")
+                frameaddr = address + frame['src'] #frame내 src값 찾아서 연결된 주소 확인하기 
+
+                res1 = requests.get(frameaddr) # frame내의 연결된 주소 읽어오기 
+                frame_soup = BeautifulSoup(res1.text, 'html.parser')
+                items = frame_soup.select('body > div > table > tbody > tr')
+
+                #bs4 요소 빼주기
+                for item in items:
+                    name = item.select('td')[0].text.replace("\n","")# 줄바꿈없애고
+                    name = name.replace("\t", "") # 탭없애고
+                    price = item.select('td')[1].text
+                    print(name + "\t" + price)
+
+            else:
+                    print("첫 화면으로 돌아갑니다")
+                    BankSystem.lobby()     
 
     class BankSystem:
         def lobby():
@@ -182,7 +238,7 @@ try:
                 print("\n======================================")
             
                 try:
-                    work = input("원하시는 번호를 입력하세요! : ")
+                    work = input("원하시는 번호를 입력하세요!   ")
                     work1= int(work)
                     if work1 >= 8:
                         raise Exception
@@ -191,29 +247,85 @@ try:
                     time.sleep(2)
                 else:
                     if work == "1":
-                        BankManager.accountCreate()
+                        print("\n============= [1] 계좌 개설 ===============")
+                        BankManager.create()
+                        print("============================================")
+                        time.sleep(2)
                     elif work == "2":
-                        BankManager.addBalance()
+                        print("\n============= [2] 입금 처리 ===============")
+                        BankManager.deposit()
+                        print("============================================")
+                        time.sleep(2)
                     elif work == "3":
-                        BankManager.withdrawBalance()
+                        print("\n============= [3] 출금 처리 ===============")
+                        BankManager.withdraw()
+                        print("============================================")
+                        time.sleep(2)
                     elif work == "4":
+                        print("\n============= [4] 잔액 조회 ===============")
                         BankManager.showBalance()
+                        print("============================================")
+                        time.sleep(5)
                     elif work == "5":
+                        print("\n============= [5] 입출금 기록 ===============")
                         BankManager.accountInfo()
-                    # elif work == "5":
-                        
+                        print("\n============================================")
+                        time.sleep(2)
+                    elif work == "6":
+                        print("\n============= [6] 환율 조회 ===============")
+                        BankManager.exchangeCurrent()
+                        print("============================================")
+                        time.sleep(5)
                     elif work == "7":
+                        print("=============================================")
                         with open('bankaccountlist.txt', 'w') as f:
                             for data in bankinform:
                                 save = str(data[0]) + ":" + str(data[1]) + ":" + str(data[2]) + "\n"
                                 f.write(save)
-                        print("종료합니다")
+                        
+                        conn = pymysql.connect(host="localhost", port=3306, db="bladb", passwd="blapw", user="blauser")
+                        cur = conn.cursor()
+                        result = pd.read_sql_query('select * from history',conn)
+                        print(result)
+                        cur.close()
+                        conn.close()          
+                        print("\n종료합니다")
                         sys.exit()
                 
-
+        def pyHistoryD(x):
+            conn = pymysql.connect(host="localhost", port=3306, db="bladb", passwd="blapw", user="blauser")
+            cur = conn.cursor()
+                
+            sql =''' 
+            insert into history(계좌번호,이름,입출금,금액,잔액)
+            values(%s,%s,%s,%s,%s);
+            '''
+            for data in bankinform:                      
+                cur.execute(sql,(str(data[0]),str(data[1]),"입금",int(x),int(data[2])))
+            conn.commit()
+            cur.close()
+            conn.close()
+        
+        
+        
+        def pyHistoryW(x):
+            conn = pymysql.connect(host="localhost", port=3306, db="bladb", passwd="blapw", user="blauser")
+            cur = conn.cursor()
+                
+            sql =''' 
+            insert into history(계좌번호,이름,입출금,금액,잔액)
+            values(%s,%s,%s,%s,%s);
+            '''
+            for data in bankinform:                      
+                cur.execute(sql,(str(data[0]),str(data[1]),"출금",int(x),int(data[2])))
+            conn.commit()
+            cur.close()
+            conn.close()
+        
 
     bankinform = []
     allAccount = []
+
 
     if __name__ == "__main__":
         if os.path.isfile('bankaccountlist.txt'): # 이전 정보 확인
